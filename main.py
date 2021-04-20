0b6b7962b247564dcd5f87da1cf784f666890f70
ylow <ylona.fabiani@etu.umontpellier.fr>
2021-04-20 10:20:51 +0200

    using evalf

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ def lins(min, max, nb_samples):
 
 
 @st.cache
-def compute_samples(input_user, lins, min, max, nb_samples):
+def compute_samples(user_input, lins, min, max, nb_samples):
     lin = lins(min, max, nb_samples)
     df = pd.DataFrame(
-        data=map(lambda x: eval(input_user), lin),  # map f(x) with the linspace
+        data=map(lambda x: user_input.evalf, lin),  # map f(x) with the linspace
         index=lin,
-        columns=[input_user])
+        columns=[user_input])
     return df
 
 
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     f_input = st.sidebar.text_input("Enter a python function:", "x*x")
     try:
         expr = sp.sympify(f_input)
-        st.sidebar.latex(f_input)
+        st.sidebar.write(sp.latex(expr))
     except SympifyError:
         st.error("An error as occurred, please try again.")
 
@@ -36,7 +36,8 @@ if __name__ == '__main__':
     x_max = st.sidebar.number_input("x_max?", step=0.5, value=5.0, help="Choose a max value for your x")
     st.write("Your x_min is: ", x_min, "Your x_max is: ", x_max)
 
-    samples = st.sidebar.number_input("Sample size?", 0, 10000, 100, help="Choose a number of samples between 0 and 10000")
+    samples = st.sidebar.number_input("Sample size?", 0, 10000, 100, help="Choose a number of samples between 0 and "
+                                                                          "10000")
 
     st.write("Chart:")
-    st.line_chart(compute_samples(f_input, lins, x_min, x_max, samples))
+    st.line_chart(compute_samples(expr, lins, x_min, x_max, samples))
